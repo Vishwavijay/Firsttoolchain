60188739e047631cf7ab7456af822ded152891c3
Vishwavijay <Vishwavijay@users.noreply.github.com>
2018-04-13 12:36:51 -0400

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
+var xy = require('../final.js');
 /* GET users listing. */
+
 router.get('/', function(req, res) {
-	var xy = require('../final.js');
-  res.send('EVS DB email verify deletion - success');
+	console.log("before cleanup evscleanup");
+var	status = xy.evscleanup();
+console.log("after cleanup evscleanup");
+	  return res.send('EVS DB email verify deletion --   '+ status);
+	 
 });
 
 module.exports = router;
